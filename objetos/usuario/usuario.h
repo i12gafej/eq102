a718c84300e4dc3293222e196fe49884d8251553
i12gafej <i12gafej@uco.es>
2022-12-05 19:25:10 +0100

    Archivos de usuario y su prueba

--- objetos/usuario/usuario.h
+++ objetos/usuario/usuario.h
@@ -7,31 +7,30 @@
 
 #ifndef USUARIO_H
 #define USUARIO_H
-
-#include <string.h>
 #include <stdlib.h>
-#include <iostream>
+#include <string>
 using namespace std;
 
 class Usuario{
-private:
-	string correo_;
-	string contra_;
-public:
-	Usuario(string correo = "empty", string contra = "empty"){};
-	//setters
-	inline void set_correo(string correo){correo_ = correo;}
-	inline void set_contra(string contra){contra_ = contra;}
-	//Getters
-	inline string get_correo()const{return correo_;}
-	inline string get_contra()const{return contra_;}
-	//User's operations
-	bool iniciarSesion();
-	void verPagina();
+	private:
+		string correo_, contra_;
+	public:
+		Usuario(string, string);
+		//setters
+		inline void set_correo(string correo){correo_ = correo;}
+		inline void set_contra(string contra){contra_ = contra;}
+		//Getters
+		inline string get_correo(){return correo_;}
+		inline string get_contra(){return contra_;}
+		//User's operations
+		bool iniciarSesion();
+		void verPagina();
 };
-bool comprobar_coincidencia();
+bool comprobar_coincidencia(string cor, string con);
 bool comprobar_correo(string cor);
-
+bool arroba(string cor);
+void imprimir();
+void menu();
 
 
 #endif /* OBJETOS_USUARIO_USUARIO_H_ */
