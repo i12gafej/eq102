ee988f004b34fddd11985bc6213320638517cced
i12gafej <i12gafej@uco.es>
2022-12-05 17:18:22 +0100

    Rep

---
+++ objetos/admin/admin.h
@@ -0,0 +1,27 @@
+/*
+ * admin.h
+ *
+ *  Created on: 5 dic 2022
+ *      Author: USUARIO
+ */
+
+#ifndef ADMIN_H
+#define ADMIN_H
+
+#include <string>
+#include <iostream>
+#include "../usuario/usuario.h"
+using namespace std;
+
+class Admin:public Usuario{
+	private:
+
+	public:
+
+};
+
+
+
+
+
+#endif /* OBJETOS_C_CURSOS_ADMIN_H_ */
