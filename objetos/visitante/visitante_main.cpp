a423a3a2cd4b8d4e1f58505f28858b8434b35d00
i12gafej <i12gafej@uco.es>
2022-12-06 12:44:35 +0100

    user and visitor

---
+++ objetos/visitante/visitante_main.cpp
@@ -0,0 +1,18 @@
+/*
+ * visitante_main.cpp
+ *
+ *  Created on: 6 dic 2022
+ *      Author: USUARIO
+ */
+#include "visitante.h"
+using namespace std;
+
+int main(){
+	Visitante guest = Visitante("null", "null");
+	guest.registrarUsuario();
+	return 0;
+}
+
+
+
+
